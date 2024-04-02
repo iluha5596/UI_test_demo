ecde673f5f8271288162129a9b64f33f14acaae9
iluha5596 <pantik96@mail.ru>
2024-04-02 16:29:07 +0300

    Добавлен скриншот при неудачном тесте

--- conftest.py
+++ conftest.py
@@ -11,3 +11,17 @@ def driver():
     driver.maximize_window()
     yield driver
     driver.quit()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    if rep.when == "call" and rep.failed:
+        try:
+            driver = item.funcargs['driver']
+        except KeyError:
+            return
+        screenshot_path = f"screenshot_failed_test/screenshot_{item.name}.png"
+        driver.save_screenshot(screenshot_path)
+        print(f"\nScreenshot saved as {screenshot_path}")
