46361b981a5f392e14f544c7efebab1d5871099c
iluha5596 <63452623+iluha5596@users.noreply.github.com>
2024-04-03 08:00:23 +0300

    Update login_page_locator.py

--- locators/login_page_locator.py
+++ locators/login_page_locator.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocator:
-    EMAIL = (By.XPATH, '//*[@id="emailф"]')
+    EMAIL = (By.XPATH, '//*[@id="email"]')
     PASSWORD = (By.XPATH, '(//input[@id="pass"])[1]')
     SIGN_IN = (By.XPATH, '//button[@class="action login primary"]')
     CUSTOMER_LOGIN = (By.XPATH, '(//*[text()="Customer Login"])[2]')
